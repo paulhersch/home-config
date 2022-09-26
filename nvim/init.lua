ebe91fea43941adb1d0688a14c7654b23a2c0a0e
paulhersch <74120050+paulhersch@users.noreply.github.com>
2022-09-26 19:54:34 +0200

    red slightly more red now, somehow Map wasnt included idk

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,11 @@
+function Map(mode, lhs, rhs, opts) --stole this from some article
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
 require ("plugins")
 require ("keybinds")
 require ("options")
